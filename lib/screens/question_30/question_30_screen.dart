9699894b0402d1b84ef1db9e6a910c5eeb287a68
marek-jakub <marekjakub@hotmail.co.uk>
2023-12-19 17:09:26 +0100

    Add question text.

--- lib/screens/question_30/question_30_screen.dart
+++ lib/screens/question_30/question_30_screen.dart
@@ -11,9 +11,47 @@ class _QuestionThirtyScreenState extends State<QuestionThirtyScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Question 30'),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Question 30'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.grey),
+              margin: const EdgeInsets.fromLTRB(0, 0, 4, 0),
+              padding: const EdgeInsets.all(7),
+              child: const Text('30'),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 7, 0, 0),
+                child: Text.rich(
+                  TextSpan(
+                    text: _getQuestion30(),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        body: const Text('Question 30 screen'));
+      ),
+    );
+  }
+
+  String _getQuestion30() {
+    String question = '''Users dislike typing, is information computed
+                   for the users where applicable?''';
+
+    final extraWhiteSpace = RegExp(r'(?! )\s+| \s+');
+    question = question.replaceAll('\n', '');
+    question = question.replaceAll(extraWhiteSpace, ' ');
+
+    return question;
   }
 }
