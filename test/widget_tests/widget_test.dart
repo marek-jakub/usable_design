8c3bdaff931a22b815f8b5af11dabfe9cb3f3024
marek-jakub <marekjakub@hotmail.co.uk>
2024-02-09 16:45:14 +0100

    Update widget tests for question 30 screen.

---
+++ test/widget_tests/widget_test.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:usable_design/main.dart';
+
+Future<void> addDelay(int ms) async {
+  await Future<void>.delayed(Duration(milliseconds: ms));
+}
+
+void main() {
+  group(
+    "Validate QuestionThirty screen",
+    () {
+      testWidgets(
+          'Entering app with welcome screen showing usability questions',
+          (WidgetTester tester) async {
+        // Build our app and trigger a frame.
+        await tester.pumpWidget(const UsabilityDesign());
+        tester.printToConsole('Starting app...');
+        //await addDelay(4000);
+        await tester.pumpAndSettle();
+
+        // Verify that the title is shown.
+        tester.printToConsole('Welcome screen testing...');
+        expect(find.text('Usability Questions'), findsOneWidget);
+        expect(find.text('1'), findsNothing);
+        await tester.pumpAndSettle();
+
+        // Verify existing questions
+        tester.printToConsole('Test visible questions.');
+        final instances = find.byType(Text);
+        expect(instances, findsAtLeast(10));
+
+        // Scroll down the list of questions
+        tester.printToConsole('Scrolling down...');
+        final lastQuestionFinder = find.byKey(const Key('question 30'));
+        await tester.scrollUntilVisible(
+          lastQuestionFinder,
+          500,
+          duration: const Duration(milliseconds: 1000),
+        );
+        final lastQuestion = lastQuestionFinder.evaluate().first;
+        expect(lastQuestion.widget.key, const ValueKey('question 30'));
+
+        await tester.tap(find.byKey(const ValueKey('question 30')));
+        await tester.pumpAndSettle();
+
+        // Verify the expected screen title.
+        tester.printToConsole('Question 30 screen testing.');
+        expect(find.text('Question 30'), findsOneWidget);
+        expect(find.text('30'), findsOneWidget);
+        expect(find.text('1'), findsNothing);
+
+        // Test checkbox list tile
+        tester.printToConsole('Testing checkbox...');
+        expect(find.byKey(const Key('EU_member')), findsOneWidget);
+
+        // Find the checkbox list tile
+        final checkboxTileFinder = find.byKey(const Key('EU_member'));
+        // Check if the checkbox is checked
+        final isCheckboxChecked =
+            tester.widget<CheckboxListTile>(checkboxTileFinder).value;
+        expect(isCheckboxChecked, false);
+        // Tap on the checkbox
+        await tester.tap(checkboxTileFinder);
+        await tester.pumpAndSettle();
+        // Check if the checkbox is now checked
+        final isCheckboxCheckedAfterTap =
+            tester.widget<CheckboxListTile>(checkboxTileFinder).value;
+        expect(isCheckboxCheckedAfterTap, true);
+
+        // Test state dropdown field
+        tester.printToConsole('Testing state dropdown...');
+        expect(find.byKey(const Key('country')), findsOneWidget);
+
+        // Test state dropdown field
+        tester.printToConsole('Testing state dropdown first item is Austria.');
+        expect(find.byKey(const Key('country')), findsOneWidget);
+        final stateDropdown = find.byKey(const Key('country'));
+        await tester.tap(stateDropdown);
+        await tester.pumpAndSettle();
+        final dropdownItem = find.text('Austria');
+        expect(dropdownItem, findsOneWidget);
+        // Tap on the dropdown item
+        await tester.tap(dropdownItem);
+        await tester.pumpAndSettle();
+
+        // Uncheck the checkbox list tile
+        tester.printToConsole('Uncheck text box.');
+        await tester.tap(checkboxTileFinder);
+        await tester.pumpAndSettle();
+
+        // Check if the checkbox is now checked
+        expect(
+            tester.widget<CheckboxListTile>(checkboxTileFinder).value, false);
+
+        // Test state dropdown field
+        tester.printToConsole('Testing state dropdown first item is Albania.');
+        expect(find.byKey(const Key('country')), findsOneWidget);
+        await tester.tap(stateDropdown);
+        await tester.pumpAndSettle();
+        expect(find.text('Albania'), findsOneWidget);
+      });
+    },
+  );
+}
